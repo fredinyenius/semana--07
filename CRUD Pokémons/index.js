1a048ee2f90a5bab80cfdaf1fda9c61d237b659b
Fredy Mamani <fredy.mamani.canahua@gmail.com>
2022-12-14 23:30:26 -0500

    Update 02

--- CRUD Pokémons/index.js
+++ CRUD Pokémons/index.js
@@ -1,20 +1,9 @@
 'use strict';
 
 const documentReady = () => {
-  const tbodyPokemons = document.querySelector('#tbodyPokemons');
-  const formPokemon = document.querySelector('#formPokemon');
-   tbodyPokemons.innerHTML += `
-  <tr>
-    <td>1</td>
-    <td>Pikachu</td>
-    <td>Electrico</td>
-    <td>250</td>
-    <td>Trueno</td>
-    <td>Voltio</td>
-    <td><img src="https://i.pinimg.com/550x/dd/20/84/dd208480c6937eb68a91f73df5ca904a.jpg" alt="Pikachu" class="img-fluid" style="max-width: 128px"></td>
-    <td>✍</td>
-</tr>
-  `;
+  const pokemons = [];
+  const formPokemon = document.getElementById('formPokemon');
+  
   const createPokemon = (e) => {
     e.preventDefault();
     const documentFormPokemon = document.forms['fromPokemon'];
@@ -24,7 +13,32 @@ const documentReady = () => {
     const attack = documentFormPokemon.attack.value;
     const special = documentFormPokemon.special.value;
     const imgUrl = documentFormPokemon.imgUrl.value;
-    console.log({ mane, type, HP, attack, special, imgUrl});
+    pokemons.push({ name, type, HP, attack, special, imgUrl});
+    readPokemons();
+  };
+   const readPokemons = () => {
+   
+    const tbodyPokemons = document.getElementById('tbodyPokemons');  
+    tbodyPokemons.innerHTML ='' ;
+    pokemons.forEach((element, index) => {
+    const{ name, type, HP, attack, special, imgUrl } = element;
+    tbodyPokemons.innerHTML +=`
+  <tr>
+    <td>${index}</td>
+    <td>${name}</td>
+    <td>${type}</td>
+    <td>${HP}</td>
+    <td>${attack}</td>
+    <td>${special}</td>
+    <td>
+    <img 
+       src="${imgUrl}"
+        alt="${name}" class="img-fluid" style="max-width: 128px">
+        </td>
+  </tr>
+ ` ;
+    });
+  
 };
 formPokemon.addEventListener('submit', createPokemon)
 };

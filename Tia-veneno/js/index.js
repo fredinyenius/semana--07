c4f0dc17049e587be729a024c88bee0094b1fec8
Fredy Mamani <fredy.mamani.canahua@gmail.com>
2022-12-16 23:31:08 -0500

    Tia venen02

--- Tia-veneno/js/index.js
+++ Tia-veneno/js/index.js
@@ -1,24 +1,17 @@
+
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
- new Swiper('.swiper', {
-    // Optional parameters
-    autoplay: {
-        delay:5000
-    },
-  
-    // If we need pagination
-    loop: true,
-    speed:500,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  
-    
-    
-  });
+new Swiper('.swiper', {
+  autoplay: {
+    delay: 5000
+  },
+  loop: true,
+  speed: 500,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
